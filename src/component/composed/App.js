7a32726a191597cac2fea40fb354ce20ac596c99
Marcel Beumer <marcel@marcelbeumer.com>
2019-01-20 20:41:08 +0100

    Update hero text

--- src/component/composed/App.js
+++ src/component/composed/App.js
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import StyleSheet, {px} from '../styles';
+import StyleSheet, { px } from '../styles';
 import SiteLogo from '../base/SiteLogo';
 import AppStoreIcon from '../base/AppStoreIcon';
 import MediumIcon from '../base/MediumIcon';
@@ -23,25 +23,25 @@ export const styles = StyleSheet.create({
     flex: 1,
 
     [theme.media.midSize]: {
-      fontSize: px(16),
-    },
+      fontSize: px(16)
+    }
   },
   content: {
     marginTop: px(60),
     maxWidth: px(500),
     [theme.media.midSize]: {
-      marginTop: px(100),
+      marginTop: px(100)
     },
     [theme.media.fullSize]: {
-      marginTop: px(200),
-    },
+      marginTop: px(200)
+    }
   },
   logo: {
     margin: '0 auto',
     width: px(225),
     [theme.media.midSize]: {
-      width: px(350),
-    },
+      width: px(350)
+    }
   },
   heroText: {
     fontSize: px(18),
@@ -50,20 +50,20 @@ export const styles = StyleSheet.create({
     [theme.media.midSize]: {
       fontSize: px(24),
       margin: `${px(theme.gutter * 6)} ${px(theme.gutter)}`,
-      lineHeight: '1.25em',
-    },
+      lineHeight: '1.25em'
+    }
   },
   footerText: {
     margin: `${px(theme.gutter)} 0`,
     [theme.media.midSize]: {
-      margin: `${px(theme.gutter * 2)} 0`,
-    },
+      margin: `${px(theme.gutter * 2)} 0`
+    }
   },
   icons: {
     flexDirection: 'row',
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   icon: {
     width: px(38),
@@ -71,8 +71,8 @@ export const styles = StyleSheet.create({
 
     [theme.media.midSize]: {
       width: px(58),
-      margin: `0 ${px(theme.gutter)}`,
-    },
+      margin: `0 ${px(theme.gutter)}`
+    }
   },
   iconWh: {
     width: px(38),
@@ -82,9 +82,9 @@ export const styles = StyleSheet.create({
     [theme.media.midSize]: {
       width: px(58),
       height: px(58),
-      margin: `0 ${px(theme.gutter)}`,
-    },
-  },
+      margin: `0 ${px(theme.gutter)}`
+    }
+  }
 });
 
 const App = () => (
@@ -94,11 +94,17 @@ const App = () => (
         <SiteLogo />
       </div>
       <p className={styles.heroText}>
-        I make apps and web frontends<br />
-        with JavaScript and React
+        JavaScript developer <br />
+        and frontend architect. <br />
+        <br />I like TypeScript, React <br />
+        and other things that are <br />
+        lean, fun and productive.
       </p>
       <div className={styles.icons}>
-        <Link href="https://itunes.apple.com/de/developer/marcel-beumer/id1175753556?l=en&mt=8" className={styles.iconWh}>
+        <Link
+          href="https://itunes.apple.com/de/developer/marcel-beumer/id1175753556?l=en&mt=8"
+          className={styles.iconWh}
+        >
           <AppStoreIcon />
         </Link>
         <Link href="https://medium.com/@marcelbeumer" className={styles.icon}>
--- src/component/theme.js
+++ src/component/theme.js
@@ -7,18 +7,19 @@ const theme = {
   textColor: '#333333',
   gutter: 10,
   media: {
+    smallSize: '@media screen and (min-width: 400px)',
     midSize: '@media screen and (min-width: 600px)',
-    fullSize: '@media screen and (min-width: 768px)',
-  },
+    fullSize: '@media screen and (min-width: 768px)'
+  }
 };
 
 // Workaround to force media queries to proper order
 StyleSheet.create({
   __: Object.keys(theme.media).reduce((p, c, i) => {
     const rules = p;
-    rules[theme.media[c]] = {fontSize: i};
+    rules[theme.media[c]] = { fontSize: i };
     return rules;
-  }, {}),
+  }, {})
 });
 
 export default theme;
